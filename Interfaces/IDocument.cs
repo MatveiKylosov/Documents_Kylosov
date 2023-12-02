c47bf10e19e68b7fce3f4f106f64d79924878df0
MatveiKylosov <matveikyl@mail.ru>
2023-12-02 22:55:24 +0500

    Реализованные интерфейсы. Kylosov

---
+++ Interfaces/IDocument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Kylosov.Interfaces
+{
+    public interface IDocument
+    {
+        void Save(bool Update = false);
+        List<Classes.DocumentContext> AllDocuments();
+        void Delete();
+    }
+}
