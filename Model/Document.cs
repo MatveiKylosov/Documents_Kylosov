4e9bda9865112fcb6a291cab6d20b3853bc2c8cb
MatveiKylosov <matveikyl@mail.ru>
2023-12-02 22:52:12 +0500

    Реализованные модели. Kylosov

---
+++ Model/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Kylosov.Model
+{
+    public class Document
+    {
+        public int id;
+        public string src;
+        public string name;
+        public string user;
+        public int id_document;
+        public DateTime date;
+        public int status;
+        public string vector;
+    }
+}
